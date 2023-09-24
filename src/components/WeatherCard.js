dfbd17807b9c5c7aa692c77cf18b4737d3e8a616
Gaurangi97 <nimashaumaya64@gmail.com>
2023-09-24 22:59:26 +0530

    Weather card style changed

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -6,27 +6,27 @@ export default function WeatherCard(props) {
       <div className="card-header bg-info text-white">Weather Details</div>
       <div className="card-body">
         <div className="card-text">
-          <p className="font-weight-bold">Temperature</p>
+          <p className="font-weight-bold mb-0">Temperature:</p>
           <p>{props.weather.main.temp} K</p>
         </div>
         <div className="card-text">
-          <p className="font-weight-bold">Feels Like</p>
+          <p className="font-weight-bold mb-0">Feels Like:</p>
           <p>{props.weather.main.feels_like} K</p>
         </div>
         <div className="card-text">
-          <p className="font-weight-bold">Humidity</p>
+          <p className="font-weight-bold mb-0">Humidity:</p>
           <p>{props.weather.main.humidity}%</p>
         </div>
         <div className="card-text">
-          <p className="font-weight-bold">Weather</p>
+          <p className="font-weight-bold mb-0">Weather:</p>
           <p>{props.weather.weather[0].description}</p>
         </div>
         <div className="card-text">
-          <p className="font-weight-bold">Country</p>
+          <p className="font-weight-bold mb-0">Country:</p>
           <p>{props.weather.sys.country}</p>
         </div>
         <div className="card-text">
-          <p className="font-weight-bold">Wind Speed</p>
+          <p className="font-weight-bold mb-0">Wind Speed:</p>
           <p>{props.weather.wind.speed} m/s</p>
         </div>
       </div>
--- src/pages/NavBar.js
+++ src/pages/NavBar.js
@@ -16,11 +16,11 @@ export default function NavBar() {
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav ms-auto">
+      {/* <ul className="navbar-nav ms-auto">
         <li className="nav-item">
           <button className="btn btn-info" onClick={handleLogout}>Logout</button>
         </li>
-      </ul>
+      </ul> */}
     </div>
   </nav>
   )
